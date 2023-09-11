9bb123a296ad8fbc0696de57afbec6f230337b45
GumSurf <gabriel.christe@gmail.com>
2023-09-11 19:05:26 +0200

    Add: file Card.tsx

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Accueil from './Accueil';
+import Card from './Card';
+
 import '../App.css';
+import '../styles/custom.css';
+import '../styles/output.css';
 
 function App() {
   return (
     <div>
         <Header />
+        <Accueil />
+        <Card />
         <Footer />
     </div>
   );
