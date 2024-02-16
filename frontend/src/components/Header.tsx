72677218c083819fcb500326ebdef17dae3b740d
GumSurf <gabriel.christe@gmail.com>
2024-02-16 20:17:16 +0100

    Add: darkmode

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from 'react'
+import { Link } from 'react-router-dom';
 import { Disclosure } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Logo from '../assets/images/Galaxy-Code.png';
@@ -14,16 +14,16 @@ function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function Header() {
     return (
-        <Disclosure as="nav" className="bg-gray-800">
+        <Disclosure as="nav" className="bg-gray-100  dark:bg-custom">
             {({ open }) => (
                 <>
                     <div className="mx-auto max-w-7x1 px-2 sm:px-6 lg:px-8">
                         <div className="relative flex h-16 items-center justify-between">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                 {/* Mobile menu button*/}
-                                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-black dark:text-white hover:bg-gray-700 dark:hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                                     <span className="absolute -inset-0.5" />
                                     <span className="sr-only">Open main menu</span>
                                     {open ? (
@@ -48,7 +48,7 @@ export default function Example() {
                                                 key={item.name}
                                                 href={item.href}
                                                 className={classNames(
-                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                    item.current ? 'bg-gray-900 text-white dark:bg-gray-700 dark:text-white' : 'text-black hover:bg-gray-300 dark:hover:bg-gray-900 hover:text-black dark:text-white',
                                                     'rounded-md px-3 py-2 text-sm font-medium'
                                                 )}
                                                 aria-current={item.current ? 'page' : undefined}
@@ -60,20 +60,24 @@ export default function Example() {
                                 </div>
                             </div>
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                            <Link to="/login">
                                 <button
                                     type="button"
-                                    className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                    className="relative rounded-full bg-gray-100 dark:bg-custom p-1 text-black dark:text-white hover:text-white dark:hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                                 >
-                                    <p className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'>Se connecter</p>
+                                    <p className='text-black dark:text-white hover:bg-gray-300 dark:hover:bg-gray-900 rounded-md px-3 py-2 text-sm font-medium'>Se connecter</p>
                                 </button>
+                            </Link>
                             </div>
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                            <Link to="/register">
                                 <button
                                     type="button"
-                                    className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                    className="relative rounded-full bg-gray-100 dark:bg-custom p-1 text-gray-400 dark:text-purple-700 hover:text-white dark:hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                                 >
-                                    <p className='text-gray-300 bg-purple-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'>S'inscrire</p>
+                                    <p className='text-white bg-purple-700 dark:bg-purple-700 hover:bg-gray-700 dark:hover:bg-gray-900 dark:text-white rounded-md px-3 py-2 text-sm font-medium'>S'inscrire</p>
                                 </button>
+                            </Link>
                             </div>
                         </div>
                     </div>
@@ -86,7 +90,7 @@ export default function Example() {
                                     as="a"
                                     href={item.href}
                                     className={classNames(
-                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                        item.current ? 'bg-gray-900 text-white dark:bg-gray-700 dark:text-white' : 'text-gray-300 hover:bg-gray-700 dark:hover:bg-gray-900 hover:text-white dark:text-white',
                                         'block rounded-md px-3 py-2 text-base font-medium'
                                     )}
                                     aria-current={item.current ? 'page' : undefined}
