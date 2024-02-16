86f6f8a01347750561e6d6a0e178752483acc6b7
GumSurf <gabriel.christe@gmail.com>
2024-02-16 20:19:54 +0100

    Add: page d'inscription

---
+++ frontend/src/components/register/SignUpPage.tsx
@@ -0,0 +1,17 @@
+// Import des modules React nécessaires
+import React from 'react';
+
+// Import des composants spécifiques à la page d'inscription
+import SignUpForm from './SignUpForm'; // Supposons que SignUpForm est le composant du formulaire d'inscription
+
+// Définition du composant de la page d'inscription
+const InscriptionPage = () => {
+  return (
+    <div>
+      <h2>Inscription</h2>
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default InscriptionPage;
