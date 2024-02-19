0563937144a3965ec0301cc87914eaf1aba90320
GumSurf <gabriel.christe@gmail.com>
2024-02-19 23:56:00 +0100

    Add: nouvelle route vers /Tutoriel

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Accueil from './Home';
 import Register from './Register';
 import Login from './Login';
+import Tutoriel from './Tutoriel';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                     <Route path="/" element={<Accueil />} />
                     <Route path='/Login' element={<Login />} />
                     <Route path="/Register" element={<Register />} />
+                    <Route path="/Tutoriel" element={<Tutoriel />} />
                 </Routes>
             </div>
         </Router>
