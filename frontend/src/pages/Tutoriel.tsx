1758c8e7fa63888749db9376e24ef971d630f4f2
GumSurf <gabriel.christe@gmail.com>
2024-02-19 23:56:21 +0100

    Add: nouvelle page pour les tutoriels

---
+++ frontend/src/pages/Tutoriel.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import LoginForm from '../components/login/LoginForm'; // Import du composant de formulaire de connexion
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import PrintTutoriel from '../components/tutoriel/PrintTutoriel';
+import DarkModeToggle from '../components/DarkMode';
+
+import { useDarkMode } from '../components/DarkModeContext';
+import '../App.css';
+import '../styles/custom.css';
+import '../styles/output.css';
+
+const LoginPage = () => {
+    const { darkMode } = useDarkMode();
+
+    return (
+        <div>
+            <Header />
+            <section className={`background-radial-gradient ${!darkMode ? 'background-radial-gradient-clair' : ''}`}>
+                <DarkModeToggle />
+                <PrintTutoriel />
+            </section>
+            <Footer />
+        </div>
+    );
+};
+
+export default LoginPage;
