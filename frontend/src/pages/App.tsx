bdb9f66e5d5c509d17e8b051f32760549b9a8852
GumSurf <gabriel.christe@gmail.com>
2024-02-26 22:26:21 +0100

    Add: beforeunload appelle disconnect

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Accueil from './Home';
 import Register from './Register';
 import Login from './Login';
 import Tutoriel from './Tutoriel';
-import Account from './Account';
+import Profile from './Profile';
+import { disconnect } from '../components/util/disconnect';
 
 function App() {
+    useEffect(() => {
+        const handleBeforeUnload = () => {
+            disconnect();
+        };
+
+        window.addEventListener('beforeunload', handleBeforeUnload);
+
+        return () => {
+            window.removeEventListener('beforeunload', handleBeforeUnload);
+        };
+    }, []);
+
     return (
         <Router>
             <div>
@@ -15,7 +28,7 @@ function App() {
                     <Route path='/Login' element={<Login />} />
                     <Route path="/Register" element={<Register />} />
                     <Route path="/Tutoriel" element={<Tutoriel />} />
-                    <Route path="/Profile" element={<Account />} />
+                    <Route path="/Profile" element={<Profile />} />
                 </Routes>
             </div>
         </Router>
