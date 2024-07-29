46e82e35956d76a1993f84adb25ef0b431fbda3c
GumSurf <gabriel.christe@gmail.com>
2024-07-29 17:26:31 +0200

    Add: route pour les pages Admin

---
+++ frontend/src/components/Route/AdminRoute.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+
+interface AdminRouteProps {
+  element: JSX.Element;
+}
+
+const AdminRoute: React.FC<AdminRouteProps> = ({ element }) => {
+  const location = useLocation(); // Utilisé pour rediriger vers la page initiale après la connexion
+  const isAuthenticated = Boolean(localStorage.getItem('token')); // Remplace par ta logique d'authentification
+
+  // Si l'utilisateur est authentifié, rendre le composant enfant
+  if (isAuthenticated) {
+    console.log("authenticated = true");
+    return element;
+  }
+
+  console.log("authenticated = false !!!!!!");
+  // Sinon, rediriger vers la page de login et mémoriser la page actuelle
+  return <Navigate to="/admin/login" state={{ from: location }} />;
+};
+
+export default AdminRoute;
