b56234b89a4993818f8275692644a83f765ec9a2
GumSurf <gabriel.christe@gmail.com>
2024-07-30 22:03:47 +0200

    Add: mise en commentaire du systéme qui supprimé le token a chaque refresh de page

--- frontend/src/components/util/layout.tsx
+++ frontend/src/components/util/layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   useEffect(() => {
     const handleBeforeUnload = () => {
-      disconnect();
+      //disconnect();
     };
 
     window.addEventListener('beforeunload', handleBeforeUnload);
