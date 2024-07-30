b19920c0d9a832316938c21ca7a363a9322bd83b
GumSurf <gabriel.christe@gmail.com>
2024-07-30 22:00:08 +0200

    Add

--- frontend/src/components/util/authAdmin.tsx
+++ frontend/src/components/util/authAdmin.tsx
@@ -16,18 +16,13 @@ export const AuthContext = createContext<AuthContextProps>({
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     const [user, setUser] = useState<any>(null);
 
-    console.log("encore avant login on sait jamais");
-
     const login = async (email: string, password: string) => {
-        console.log("dans login");
 
         try {
-            console.log("dans try");
             const response = await axios.post('http://localhost:5678/auth/login', { email, password });
             const { access_token } = response.data;
             setUser({ email, token: access_token });
             localStorage.setItem('token', access_token);
-            console.log("token = ", access_token);
         } catch (error) {
             console.error("Erreur lors de la connexion :", error);
         }
