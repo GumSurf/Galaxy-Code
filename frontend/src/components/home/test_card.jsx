9450c73a42d3468910a06d8ff7e244b2098749f2
GumSurf <gabriel.christe@gmail.com>
2024-07-30 21:58:57 +0200

    Add

--- frontend/src/components/home/test_card.jsx
+++ frontend/src/components/home/test_card.jsx
@@ -8,7 +8,7 @@ export default function CardWithActionButton() {
 
     useEffect(() => {
         // Faire une requête à votre backend pour récupérer les données des tutoriels
-        fetch('http://localhost:5678/tutorials')
+        fetch('http://localhost:5678/api/tutoriels')
             .then(response => response.json())
             .then(data => {
                 // Mettre à jour le state avec les données récupérées
@@ -25,15 +25,13 @@ export default function CardWithActionButton() {
             <div className='flex justify-center space-x-12 mb-8'>
                 {/* Mapper sur les données des tutoriels pour afficher les cartes */}
                 {tutorials.map(tutorial => (
-                    <Card key={tutorial.id} className="max-w-sm">
+                    <Card key={tutorial._id} className="max-w-sm">
                         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                             {tutorial.title}
                         </h5>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">
-                            {tutorial.text}
-                        </p>
+                        <p className="font-normal text-gray-700 dark:text-gray-400">{tutorial.content.slice(0, 100)}...</p>
                         <Button>
-                            <a href={tutorial.link}>Lire la suite</a>
+                            <a href={"tutoriel/" + tutorial._id}>Lire la suite</a>
                         </Button>
                     </Card>
                 ))}
