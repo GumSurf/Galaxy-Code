43d9ca08ff7b9915fdb27d3a3aef6c0fc6ed683e
GumSurf <gabriel.christe@gmail.com>
2024-07-30 22:01:45 +0200

    Add: meilleur fonction de Login, Logout et getToken

---
+++ frontend/src/components/util/AuthService.ts
@@ -0,0 +1,24 @@
+export const AuthService = {
+    login: async (email: string, password: string) => {
+        const response = await fetch('localhost:5678/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
+        const data = await response.json();
+        if (response.ok) {
+            localStorage.setItem('token', data.token);
+            return data.token;
+        } else {
+            throw new Error(data.message);
+        }
+    },
+    logout: () => {
+        localStorage.removeItem('token');
+    },
+    getToken: () => {
+        return localStorage.getItem('token');
+    }
+};
