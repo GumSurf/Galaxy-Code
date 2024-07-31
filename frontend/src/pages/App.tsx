ba282861ecfa8cab303cb23384b0ebbfbd42cd65
GumSurf <gabriel.christe@gmail.com>
2024-07-31 17:23:47 +0200

    Add: Switch au lieu de Routes

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HashRouter as Switch } from 'react-router-dom';
 import { AuthProvider } from '../components/util/authAdmin';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -22,7 +21,7 @@ function App() {
             <AuthProvider>
                 <Layout>
                     <Header />
-                    <Switch>
+                    <Routes>
                         <Route path={APP_ROUTES.HOME} element={<Accueil />} />
                         <Route path={APP_ROUTES.SIGN_IN} element={<Login />} />
                         <Route path={APP_ROUTES.SIGN_UP} element={<Register />} />
@@ -31,7 +30,7 @@ function App() {
                         <Route path={APP_ROUTES.PROFILE} element={<Profile />} />
                         <Route path={APP_ROUTES.ADMIN_LOGIN} element={<AdminLoginPage />} />
                         <Route path={APP_ROUTES.ADMIN_DASHBOARD} element={<AdminRoute element={<AdminDashboard />} />} />
-                    </Switch>
+                    </Routes>
                     <Footer />
                 </Layout>
             </AuthProvider>
