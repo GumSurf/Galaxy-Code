2c9047985b7b33984271a054e80e244401a73203
GumSurf <gabriel.christe@gmail.com>
2024-07-31 16:23:08 +0200

    Edit: Url

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,7 +5,7 @@ import Logo from '../assets/images/Galaxy-Code.png';
 import { isUserLoggedIn, logoutUser } from './util/auth';
 
 const navigation = [
-    { name: 'Tutoriel', href: '/Galaxy-Code/tutoriel', current: true },
+    { name: 'Tutoriel', href: '/tutoriel', current: true },
     { name: 'Cours', href: '#', current: false },
     { name: 'Blog', href: '#', current: false },
     { name: 'Forum', href: '#', current: false },
@@ -16,16 +16,16 @@ function classNames(...classes: string[]) {
 }
 
 export default function Header() {
-    const acceuille = '/Galaxy-Code';
+    const acceuille = '/';
     const loginText = isUserLoggedIn() ? 'Mon profil' : 'Se connecter';
-    const loginLink = isUserLoggedIn() ? '/Galaxy-Code/profile' : '/Galaxy-Code/login';
+    const loginLink = isUserLoggedIn() ? '/profile' : '/login';
     const registerText = isUserLoggedIn() ? 'Déconnécté' : 'S\'inscrire';
-    const registerLink = isUserLoggedIn() ? '/Galaxy-Code/deconecte' : '/Galaxy-Code/register';
+    const registerLink = isUserLoggedIn() ? '/deconecte' : '/register';
     const navigate = useNavigate();
 
     const handleLogout = () => {
         logoutUser();
-        navigate('/Galaxy-Code');
+        navigate('/');
     };
 
     return (
