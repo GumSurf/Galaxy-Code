07aa66eb44d1b26c1bba430cf2748b537c66e03c
GumSurf <gabriel.christe@gmail.com>
2024-08-16 12:39:37 +0200

    Add: new url

--- frontend/src/components/home/test_card.jsx
+++ frontend/src/components/home/test_card.jsx
@@ -1,21 +1,18 @@
-// Assurez-vous d'importer useEffect et useState si vous utilisez React functionnal components
 import React, { useEffect, useState } from 'react';
 import { Button, Card } from 'flowbite-react';
+import { API_ROUTES } from '../util/constant';
 
 export default function CardWithActionButton() {
-    // Définir un state pour stocker les données des tutoriels
     const [tutorials, setTutorials] = useState([]);
 
     useEffect(() => {
-        // Faire une requête à votre backend pour récupérer les données des tutoriels
-        fetch('https://galaxy-code-backend.vercel.app/api/tutoriels')
+        fetch(`${API_ROUTES.TUTORIELS}`)
             .then(response => response.json())
             .then(data => {
-                // Mettre à jour le state avec les données récupérées
                 setTutorials(data.slice(0, 3)); // Pour afficher seulement les 3 derniers tutoriels
             })
             .catch(error => console.error('Error fetching tutorials:', error));
-    }, []); // Le tableau vide assure que useEffect ne se déclenche qu'une seule fois lors du montage du composant
+    }, []);
 
     return (
         <div className="container my-24 mx-auto md:px-6">
@@ -23,7 +20,6 @@ export default function CardWithActionButton() {
                 Tutorials
             </h2>
             <div className='flex justify-center space-x-12 mb-8'>
-                {/* Mapper sur les données des tutoriels pour afficher les cartes */}
                 {tutorials.map(tutorial => (
                     <Card key={tutorial._id} className="max-w-sm">
                         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
