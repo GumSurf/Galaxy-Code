abfe73da6fb463fa25e21c794c1e4fca5d05667c
GumSurf <gabriel.christe@gmail.com>
2024-08-20 20:38:56 +0200

    Add: retour à la ligne et affichage du premier paraghraphe

--- frontend/src/components/tutoriel/PrintTutoriel.jsx
+++ frontend/src/components/tutoriel/PrintTutoriel.jsx
@@ -3,6 +3,21 @@ import { Card, Button } from 'flowbite-react';
 import { getTutoriels } from '../util/common';
 import { Link } from 'react-router-dom';
 
+// Fonction pour tronquer le texte
+const truncateText = (text, maxLength) => {
+    if (text.length <= maxLength) return text;
+    return text.slice(0, maxLength) + '...';
+};
+
+const replaceLineBreaks = (text) => {
+    return text.split('\n').map((item, index) => (
+        <React.Fragment key={index}>
+            {item}
+            <br />
+        </React.Fragment>
+    ));
+};
+
 export default function TutorialsPage() {
     const [tutorials, setTutorials] = useState([]);
 
@@ -21,32 +36,33 @@ export default function TutorialsPage() {
         getTutorielsList();
     }, []);
 
-    console.log("tutorials = ", tutorials);
-
     return (
         <div className="container mt-0 mb-0 min-h-screen my-24 mx-auto md:px-6">
             <h2 className="mb-12 text-center text-3xl font-bold dark:text-gray-100">
                 Tous les Tutoriels
             </h2>
             <div className='flex justify-center flex-wrap gap-8'>
-                {tutorials.map(tutorial => (
-                    <Card key={tutorial._id} className="max-w-sm">
-                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            {tutorial.title}
-                        </h5>
-                        <div className="font-normal text-gray-700 dark:text-gray-400">
-                            {tutorial.content.map((paragraph, index) => (
-                                <p key={index}>
-                                    {paragraph.content}
-                                </p>
-                            ))}
-                        </div>
-                        <Button>
-                            <Link to={`/tutoriel/${tutorial._id}`}>Lire la suite</Link>
-                        </Button>
-                    </Card>
-                ))}
+                {tutorials.map(tutorial => {
+                    // Obtenez le premier paragraphe ou une chaîne vide s'il n'y a pas de contenu
+                    const firstParagraph = tutorial.content.length > 0 ? tutorial.content[0].content : '';
+                    // Tronquez le premier paragraphe pour l'affichage
+                    const excerpt = truncateText(firstParagraph); // Ajustez la longueur selon vos besoins
+
+                    return (
+                        <Card key={tutorial._id} className="max-w-sm">
+                            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                {tutorial.title}
+                            </h5>
+                            <div className="font-normal text-gray-700 dark:text-gray-400">
+                                {replaceLineBreaks(excerpt)}
+                            </div>
+                            <Button>
+                                <Link to={`/tutoriel/${tutorial._id}`}>Lire la suite</Link>
+                            </Button>
+                        </Card>
+                    );
+                })}
             </div>
         </div>
-    )
+    );
 }
